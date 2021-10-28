bbbcc616e8963b30593a3b52cb3e13fab2d77323
Anastasiya Ostertag <nastasja83@mail.ru>
2021-10-28 22:57:38 +0700

    create jsonParse in parsers.php

---
+++ src/parsers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Differ\Differ;
+
+function jsonParse($file)
+{
+    $data = json_decode($file, null, 512, JSON_OBJECT_AS_ARRAY);
+    if (json_last_error() !== 0) {
+        return json_last_error_msg();
+    }
+    return $data;
+}

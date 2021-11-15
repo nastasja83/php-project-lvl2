f4bfb34a4bf7ccdfaf812b67dbcb5aa6540ce70b
Anastasiya Ostertag <nastasja83@mail.ru>
2021-11-15 20:01:37 +0700

    add test for Gendiff yaml format

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -10,16 +10,28 @@ class GendiffTest extends TestCase
 {
     private string $path = __DIR__ . "/fixtures/";
 
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
     private function getFilePath(string $name): string
     {
         return $this->path . $name;
     }
 
+    /**
+     * @return void
+     */
     public function testGendiffPlain(): void
     {
         $expectedPlain = file_get_contents($this->getFilePath('plain.txt'));
-        $firstPath = $this->getFilePath('first.json');
-        $secondPath = $this->getFilePath('second.json');
-        $this->assertEquals($expectedPlain, genDiff($firstPath, $secondPath));
+        $firstPathJson = $this->getFilePath('first.json');
+        $secondPathJson = $this->getFilePath('second.json');
+        $firstPathYaml = $this->getFilePath('first.yml');
+        $secondPathYaml = $this->getFilePath('second.yml');
+        $format = 'stylish';
+        $this->assertEquals($expectedPlain, genDiff($firstPathJson, $secondPathJson, $format));
+        $this->assertEquals($expectedPlain, genDiff($firstPathYaml, $secondPathYaml, $format));
     }
 }

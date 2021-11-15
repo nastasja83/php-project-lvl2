1b74a87f7d4302f4cc53eb5f7b95760f108a572f
Anastasiya Ostertag <nastasja83@mail.ru>
2021-11-15 20:04:51 +0700

    change function getDifferTree, arrays to objects

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -3,43 +3,41 @@
 namespace Differ\Differ;
 
 use Exception;
+use stdClass;
 
-use function Differ\Differ\Formatters\toString;
-use function Differ\Differ\Formatters\format;
+use function Differ\Differ\formatDiff;
 
 /**
- * Get differTree array
-
- * @param array<mixed> $config1 array
- * @param array<mixed> $config2 array
-
- * @return array<mixed>
+ * @param object $config1
+ * @param object $config2
+ *
+ * @return array
  */
-function getDifferTree(array $config1, array $config2): array
+function getDifferTree(object $config1, object $config2): array
 {
-    $mergedKeys = array_keys(array_merge($config1, $config2));
+    $mergedKeys = array_keys(array_merge((array) $config1, (array) $config2));
     sort($mergedKeys);
 
     return array_map(function ($key) use ($config1, $config2) {
-        if (!array_key_exists($key, $config1)) {
+        if (!property_exists($config1, $key)) {
             return ['key' => $key,
-                    'value' => toString($config2[$key]),
+                    'value' => $config2->{$key},
                     'type' => 'added'];
         }
-        if (!array_key_exists($key, $config2)) {
+        if (!property_exists($config2, $key)) {
             return ['key' => $key,
-                    'value' => toString($config1[$key]),
+                    'value' => $config1->{$key},
                     'type' => 'deleted'];
         }
-        if ($config1[$key] === $config2[$key]) {
+        if ($config1->{$key} === $config2->{$key}) {
             return ['key' => $key,
-                    'value' => toString($config1[$key]),
+                    'value' => $config1->{$key},
                     'type' => 'unchanged'];
         }
-        if ($config1[$key] !== $config2[$key]) {
+        if ($config1->{$key} !== $config2->{$key}) {
             return ['key' => $key,
-                    'oldValue' => toString($config1[$key]),
-                    'newValue' => toString($config2[$key]),
+                    'oldValue' => $config1->{$key},
+                    'newValue' => $config2->{$key},
                     'type' => 'changed'];
         }
     }, $mergedKeys);
@@ -48,7 +46,6 @@ function getDifferTree(array $config1, array $config2): array
 function getContent(string $pathToFile): string
 {
     $data = file_get_contents($pathToFile);
-
     if (is_string($data)) {
         return $data;
     } else {
@@ -56,11 +53,19 @@ function getContent(string $pathToFile): string
     }
 }
 
-function genDiff(string $pathToFile1, string $pathToFile2): string
+function getFileData(string $pathToFile): object
 {
-    $config1 = jsonParse(getContent($pathToFile1));
-    $config2 = jsonParse(getContent($pathToFile2));
+    $content = getContent($pathToFile);
+    $type = pathinfo($pathToFile, PATHINFO_EXTENSION);
+
+    return parse($content, $type);
+}
+
+function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
+{
+    $config1 = getFileData($pathToFile1);
+    $config2 = getFileData($pathToFile2);
     $differTree = getDifferTree($config1, $config2);
 
-    return format($differTree);
+    return formatDiff($format, $differTree);
 }
